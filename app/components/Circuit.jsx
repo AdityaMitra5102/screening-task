e9d777a25bba99a7495d65ebecfcfad135194c33
Aditya Mitra <adityaarghya0@gmail.com>
2025-07-01 01:01:40 +0530

    Update Circuit.jsx

--- app/components/Circuit.jsx
+++ app/components/Circuit.jsx
@@ -231,7 +231,7 @@ const gridKeyElement = <p style={{display: 'none'}} id="grid-key-debug">{gridKey
                 overflow: 'hidden',
             }}
         >
-            {gridKeyElement}
+            
             <div id='metadata' style={{display: 'none'}}>
                 <p id='idstate'></p>
                 <p id='idChange'></p>
@@ -249,6 +249,7 @@ const gridKeyElement = <p style={{display: 'none'}} id="grid-key-debug">{gridKey
                 }></button>
             </div>           
             <ReactGridLayout
+                {...gridKeyElement}
                 allowOverlap={false}
                 layout={layout}
                 useCSSTransforms={false}
